1e2723fa0bfda45bad70924bb33e61032bf559d6
manjreka <offo9898@gmail.com>
2025-03-21 14:38:59 +0530

    env files update

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfsxVL2fnLUVI7ZNhYzs83uYYZ_2Lucdw",
-  authDomain: "eventmanagementapp-fa984.firebaseapp.com",
-  projectId: "eventmanagementapp-fa984",
-  storageBucket: "eventmanagementapp-fa984.appspot.com",
-  messagingSenderId: "690627647944",
-  appId: "1:690627647944:web:6049cee522752c16f408fd",
-  measurementId: "G-24Q8SX084G",
+  apiKey:
+    process.env.REACT_APP_FIREBASE_API_KEY ||
+    "AIzaSyCfsxVL2fnLUVI7ZNhYzs83uYYZ_2Lucdw",
+  authDomain:
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+    "eventmanagementapp-fa984.firebaseapp.com",
+  projectId:
+    process.env.REACT_APP_FIREBASE_PROJECT_ID || "eventmanagementapp-fa984",
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
+    "eventmanagementapp-fa984.appspot.com",
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "690627647944",
+  appId:
+    process.env.REACT_APP_FIREBASE_APP_ID ||
+    "1:690627647944:web:6049cee522752c16f408fd",
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-24Q8SX084G",
 };
 
 const app = initializeApp(firebaseConfig);
